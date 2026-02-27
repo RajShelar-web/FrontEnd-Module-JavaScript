4780ee18ce6cbcb9332bb3e72522403bc2b7eabb
RajShelar-web <rajshelar390@gmail.com>
2026-02-27 06:29:25 +0000

    operators

---
+++ Operators.js
@@ -0,0 +1,50 @@
+// let name = "RAJ"
+// let name2= "Shelar"
+// console.log("i will " + (name)+ (name2))
+
+// console.log("7"-"abc") not a number
+
+// let a = "1"
+// console.log(a)
+// console.log(+a) converts string in number 
+
+// extra information
+// console.log(+false) 0
+// console.log(+true) 1
+
+// ASSIGNMENT OPRATORS
+// let a = 1
+// let b = 2
+// let c = 3 - (a=b+1)
+// console.log(a) 3
+// console.log(b) 2
+// console.log(c) 0
+
+// let a , b , c
+// a = b = c = 2+2 if we want to write in one line
+
+// let n = 2
+// n *=3+5
+// console.log(n) 16
+
+// Note the precedence of increment and the decrement opertor is higher then arithematic operators
+
+// let totalPrice = 700
+// let dis=15
+// let x=(totalPrice*dis/100)
+// let y = totalPrice-x
+// console.log(x)
+// console.log(y)
+
+// let salary = 50000
+// let inc = (15 *50000)/100
+// let ded = (10*50000)/100
+// console.log(inc)
+// console.log(ded)
+// console.log(salary+inc-ded)
+
+
+
+
+
+
