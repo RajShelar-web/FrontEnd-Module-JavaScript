d2846b2d718d5c4aeebece99d6beb654850035fa
RajShelar-web <rajshelar390@gmail.com>
2026-02-27 06:17:19 +0000

    Math Functions

---
+++ Math.js
@@ -0,0 +1,58 @@
+// Maths
+// Math in js is a building object that provides mathematical constants & functions
+// examples: PI = 3.14 --> constant
+
+// *******************************************************************************************
+
+// Mathematical Functions
+
+// *******************************************************************************************
+
+// 1) Math.round = Rounds the number to the nearest integer
+// console.log(Math.round(4.5))  5
+// console.log(Math.round(4.4))  4
+// console.log(Math.round(-4.3)) 3 (in negative integer (-4.5)also give integer 4 only after)
+
+// *******************************************************************************************
+
+// 2) Math.floor = Rounds the number DOWN to the nearest integer
+// console.log(Math.floor(4.5))  4
+// console.log(Math.floor(4.9))  4
+// console.log(Math.floor(-4.3)) 5   
+
+// *******************************************************************************************
+
+// 3) Math.ceil = Rounds the number up to the nearest integer 
+// console.log(Math.ceil(7.2)) 8
+// console.log(Math.ceil(7.9)) 8
+// console.log(Math.ceil(-4.9))  -4
+
+// *******************************************************************************************
+
+// 4) Math.tofixed =  formats a number to a specified no of decimal places & returns strings
+// let num = 5.8735726
+// console.log(num)                
+// console.log(num.toFixed(1))
+// console.log(num.toFixed(2))
+// console.log(num.toFixed(3))
+// console.log(num.toFixed(4))
+
+// **********************************************************************************************
+
+// 5) Math.ParseInt = parses a string & returns a integer (int number)
+// console.log("44")  it gives string
+// console.log(parseInt("44")) it gives integer (number)
+// console.log(parseInt("-44.99")) just gives number not string 
+
+// *************************************************************************************************
+
+// 5) parseFloat = parses a string & returns a floating point number 
+
+// console.log(parseFloat("3.1555"))
+// console.log(parseFloat("-34.00"))
+
+
+
+
+
+// console.log(Math) Object
